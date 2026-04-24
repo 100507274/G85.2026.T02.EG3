c79c320c027b72b8f56cb802d9b61b318b11dad5
100550137 <100550137@alumnos.uc3m.es>
2026-04-24 22:46:17 +0200

    borro validate_cif(cif) y hago test para comprobar que funciona

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -17,51 +17,6 @@ class EnterpriseManager:
     def __init__(self):
         pass
 
-    @staticmethod
-    def validate_cif(cif: str):
-        """validates a cif number """
-        if not isinstance(cif, str):
-            raise EnterpriseManagementException("CIF code must be a string")
-        cif_patrón = re.compile(r"^[ABCDEFGHJKNPQRSUVW]\d{7}[0-9A-J]$")
-        if not cif_patrón.fullmatch(cif):
-            raise EnterpriseManagementException("Invalid CIF format")
-
-        cif_letra_inicial = cif[0]
-        cif_intermedio = cif[1:8]
-        cif_control = cif[8]
-
-        cif_sum_impar = 0
-        cif_sum_par = 0
-
-        for i in range(len(cif_intermedio)):
-            if i % 2 == 0:
-                x = int(cif_intermedio[i]) * 2
-                if x > 9:
-                    cif_sum_impar = cif_sum_impar + (x // 10) + (x % 10)
-                else:
-                    cif_sum_impar = cif_sum_impar + x
-            else:
-                cif_sum_par = cif_sum_par + int(cif_intermedio[i])
-
-        cif_suma_total = cif_sum_impar + cif_sum_par
-        cif_resta = cif_suma_total % 10
-        cif_resta_control = 10 - cif_resta
-
-        if cif_resta_control == 10:
-            cif_resta_control = 0
-
-        cif_diccionario = "JABCDEFGHI"
-
-        if cif_letra_inicial in ('A', 'B', 'E', 'H'):
-            if str(cif_resta_control) != cif_control:
-                raise EnterpriseManagementException("Invalid CIF character control number")
-        elif cif_letra_inicial in ('P', 'Q', 'S', 'K'):
-            if cif_diccionario[cif_resta_control] != cif_control:
-                raise EnterpriseManagementException("Invalid CIF character control letter")
-        else:
-            raise EnterpriseManagementException("CIF type not supported")
-        return True
-
     def validate_starting_date(self, fecha):
         """validates the  date format  using regex"""
         my_date=self._validación_de_fecha(fecha)
