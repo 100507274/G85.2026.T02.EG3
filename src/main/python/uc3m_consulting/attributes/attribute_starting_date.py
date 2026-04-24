d82de915f22ca6e078c4d4c7435900f0a1c89400
100550137 <100550137@alumnos.uc3m.es>
2026-04-24 22:57:38 +0200

    he metido la validacion de fecha dentro del atributestartingdate para luego moverlo, solo lo hago para poder borrar el codigo en enterprisemanager

--- src/main/python/uc3m_consulting/attributes/attribute_starting_date.py
+++ src/main/python/uc3m_consulting/attributes/attribute_starting_date.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime, timezone
 from uc3m_consulting.attributes.attribute import Attribute
 from uc3m_consulting.enterprise_management_exception import EnterpriseManagementException
@@ -16,3 +17,14 @@ class AttributeStartingDate(Attribute):
         if my_date.year < 2025 or my_date.year > 2050:
             raise EnterpriseManagementException("Invalid date format")
         return fecha
+
+    def _validación_de_fecha(self, date_str):
+        fecha_patrón = re.compile(r"^(([0-2]\d|3[0-1])\/(0\d|1[0-2])\/\d\d\d\d)$")
+        fecha_valida = fecha_patrón.fullmatch(date_str)
+        if not fecha_valida:
+            raise EnterpriseManagementException("Invalid date format")
+
+        try:
+            return datetime.strptime(date_str, "%d/%m/%Y").date()
+        except ValueError as ex:
+            raise EnterpriseManagementException("Invalid date format") from ex
